4de384099d2c2fca9f31b3f7450fc7f8b36f4379
Sven Osterwalder <sven.osterwalder@gmail.com>
2016-04-19 10:36:50 +0200

    Add project-sepcific ignores for git

---
+++ src/common.hpp
@@ -0,0 +1,13 @@
+//
+//  common.h
+//  MTE7102
+//
+//  Created by Sven Osterwalder on 18/04/16.
+//
+//
+
+#ifndef common_h
+#define common_h
+
+
+#endif /* common_h */
---
+++ src/glshaderparameter.cpp
@@ -0,0 +1,9 @@
+//
+//  glshaderparameter.cpp
+//  MTE7102
+//
+//  Created by Sven Osterwalder on 18/04/16.
+//
+//
+
+#include <stdio.h>
