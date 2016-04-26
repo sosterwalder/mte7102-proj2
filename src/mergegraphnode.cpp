47f8a51bcfcd118a2c352a6b7bfae0b220271e43
Sven Osterwalder <sven.osterwalder@gmail.com>
2016-04-26 17:07:53 +0200

    Implement MergeGraphNode class

--- src/mergegraphnode.cpp
+++ src/mergegraphnode.cpp
@@ -1,9 +1,59 @@
-//
-//  mergegraphnode.cpp
-//  MTE7102
-//
-//  Created by Sven Osterwalder on 25/04/16.
-//
-//
+#include "common.hpp"
+#include "source.hpp"
+#include "sink.hpp"
+#include "graph.hpp"
+#include "mergegraphnode.hpp"
 
-#include <stdio.h>
+
+NAMESPACE_BEGIN(QCE);
+
+MergeGraphNode::MergeGraphNode(Widget *parent, Graph *parentGraph, const std::string &title) :
+    GraphNode(parent, parentGraph, title)
+{
+    Source *source = new Source(this, mParentGraph, "Output");
+    source->setId(fmt::format("{}GraphNodeSource", title));
+    addSource(source);
+    
+    mNumInputs = 1;
+    Sink *input = new Sink(this, mParentGraph, fmt::format("Input {}", mNumInputs));
+    input->setId(fmt::format("{}GraphNodeInput{}", title, mNumInputs));
+    addSink(input);
+}
+
+bool MergeGraphNode::sinkConnectedEvent(Connector *sink)
+{
+    GraphNode::sinkConnectedEvent(sink);
+    
+    mNumInputs++;
+    Sink *input = new Sink(this, mParentGraph, fmt::format("Input {}", mNumInputs));
+    input->setId(fmt::format("{}GraphNodeInput{}", mTitle, mNumInputs));
+    input->setIndex(mNumInputs);
+    addSink(input);
+    
+    return true;
+}
+
+std::string MergeGraphNode::calculateOutput()
+{
+    spdlog::get("qde")->debug("Node '{}': Calculating output", mId);
+    return GraphNode::calculateOutput();
+    
+    /*
+    
+    fmt::MemoryWriter out;
+    
+    for (auto iter = mInputs.begin(); iter != mInputs.end(); ++iter) {
+        auto sink = *iter;
+        
+        if (sink->isConnected()) {
+            auto link = sink->link();
+            auto source =
+            
+        }
+    }
+    
+    return out.str();
+     */
+}
+
+NAMESPACE_END(QCE);
--- src/mergegraphnode.hpp
+++ src/mergegraphnode.hpp
@@ -1,3 +1,15 @@
 #pragma once
 
-#
+#include "graphnode.hpp"
+
+NAMESPACE_BEGIN(QCE);
+
+class MergeGraphNode : public GraphNode
+{
+public:
+    MergeGraphNode(Widget *parent, Graph *parentGraph, const std::string &title = "Merge");
+    virtual bool sinkConnectedEvent(Connector *sink);
+    virtual std::string calculateOutput();
+};
+
+NAMESPACE_END(QCE);
