588c3487cfa615359910ae4503b06cc581e4a405
Sven Osterwalder <sven.osterwalder@gmail.com>
2016-08-04 00:10:51 +0200

    Fix pdflatex path when debugging LaTeX output

--- doc/doc/pdflatex-color.sh
+++ doc/doc/pdflatex-color.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-GREP="grep --color=always --line-buffered"
-(/usr/bin/pdflatex -file-line-error $* 2>&1) \
+GREP="ggrep --color=always --line-buffered"
+(/usr/local/bin/pdflatex -file-line-error $* 2>&1) \
  | $GREP -vP "^\s*((\[|\]|\(|\))\s*)+$" \
  | GREP_COLOR="01;31" $GREP -P "(^[^:]*:\d+: )|(^l\.\d+ )|(^! LaTeX Error: )|$" \
  | GREP_COLOR="00;33" $GREP -P "(LaTeX Warning(:|))|(Package [^\s]+ Warning(:|))|$" \
